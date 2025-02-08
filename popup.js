f1f0202e71e8584916d13286771bd36e0bdccf31
Hristian Tountchev <htoun2@uic.edu>
2025-02-07 22:21:22 -0600

    early stages for css and html for example

---
+++ popup.js
@@ -0,0 +1,8 @@
+document.getElementById("applyFilter").addEventListener("click", () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.scripting.executeScript({
+            target: { tabId: tabs[0].id },
+            function: applyFilters
+        });
+    });
+});
