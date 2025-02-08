dc66a33e026f11dd74bd89c7b9a89f501c06b198
Martin Gawron <martingaw11@gmail.com>
2025-02-08 13:53:33 -0600

    refactor api call to be http request

--- popup.js
+++ popup.js
@@ -13,9 +13,10 @@ const startChainOfEvents = (userPrompt) => {
     const feedback = document.getElementById("feedback");
     if (userPrompt == "") {
         feedback.textContent = "I need a prompt before I generate your filters... :)";
+        feedback.style.color = "#B22222"
         return;
     } 
-
+    feedback.style.color = "#222"
     feedback.textContent = "Working on those filters for you... :)";
     console.log(userPrompt);
 };
