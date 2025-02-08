a21e7a574bd827a13022490b2db1e8adc70cba5e
Martin Gawron <martingaw11@gmail.com>
2025-02-08 13:19:20 -0600

    finalize gpt api call function

--- scripts.js
+++ scripts.js
@@ -10,6 +10,8 @@ const openai = new OpenAI({
 // current system prompt for getting what we need from the AI
 const systemPrompt = `You are an expert job recruiter. You will be given a job description and you will return a JSON object with filters as described by this JSON:
 {
+  responseType: "accepted",
+  jobTitle: "from the prompt extract the most relevant job title to search by for the user as a string",
   sortBy: "choose one: recent, relevant",
   datePosted: "choose one: any time, past month, past week, past 24 hours",
   experienceLevel: "choose any number (empty for none): internship, entry level, associate, mid senior level, director, executive",
@@ -24,24 +26,6 @@ const systemPrompt = `You are an expert job recruiter. You will be given a job d
   benefits: "choose any number (empty for none): medical, vision, dental, 401k, pension, paid maternity leave, paid paternity leave, commuter benefits, student loan assistance, tuition assistance, disability",
   commitments: "choose any number (empty for none): career growth, diversity and inclusion, environmental sustainability, social impact, work life balance"
 }`
-
-// Function to call OpenAI with the prompt and return the response message.
-async function getFilterValues(prompt) {
-    try {
-        const completion = await openai.chat.completions.create({
-            model: "gpt-4o",
-            store: true,
-            messages: [{ role: "user", content: prompt }],
-        });
-        // Log the full response for debugging.
-        console.log("OpenAI full response:", completion);
-        return completion.choices[0].message.content;
-    }
-    catch (error) {
-        console.error("Error calling OpenAI:", error);
-        return JSON.stringify({ salary: "60-120", distance: 10, experience: 3 });
-    }
-}
 
 // given a user prompt return a json object with the filters
 export const getFilterJSON = async (prompt) => {
@@ -68,14 +52,28 @@ export const getFilterJSON = async (prompt) => {
     return completeion.choices[0].message.content;
   }
   catch (error) {
-    console.error("Error calling OpenAI:", error);
+    console.error("Error calling OpenAI: ", error);
     // return the default filters if the call fails
     return ({
-
+      "responseType": "error",
+      "jobTitle": "Janitor",
+      "sortBy": "recent",
+      "datePosted": "anytime",
+      "experienceLevel": [],
+      "jobType": [],
+      "remote": [],
+      "easyApply": false,
+      "hasVerification": false,
+      "underTenApplicants": false,
+      "inYourNetwork": false,
+      "fairChanceEmployer": false,
+      "salary": "",
+      "benefits": [],
+      "commitments": []
     })
   }
 };
 
 // test out the api call
-// let userPrompt = `I am a 3rd year CS student looking for internships this summer, I want some pretty relevant job postings for entry level internships to gain experience in the work place. Benefits are not that important to me but I do want to be in an environment that harbors growth for its employees. I don't mind the job being remote but I work better in an in-person environment and would like some posting that make it easy for me to apply through linkedin, money is not a big factor for me as I am just looking for some experience.`
+// let userPrompt = `I am a 3rd year CS student looking for software developer internships this summer, I want some pretty relevant job postings for entry level internships to gain experience in the work place. Benefits are not that important to me but I do want to be in an environment that harbors growth for its employees. I don't mind the job being remote but I work better in an in-person environment and would like some posting that make it easy for me to apply through linkedin, money is not a big factor for me as I am just looking for some experience.`
 // console.log(await getFilterJSON(userPrompt));
